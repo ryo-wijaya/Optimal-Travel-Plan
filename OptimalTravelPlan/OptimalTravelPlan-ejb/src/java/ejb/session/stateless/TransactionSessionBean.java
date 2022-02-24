ee08049f3aa1e07b3fdab54d5f8680210e3984ee
jordan <jordanwwb29@gmail.com>
2022-02-25 00:28:32 +0800

    payment account updated delete method

--- OptimalTravelPlan/OptimalTravelPlan-ejb/src/java/ejb/session/stateless/TransactionSessionBean.java
+++ OptimalTravelPlan/OptimalTravelPlan-ejb/src/java/ejb/session/stateless/TransactionSessionBean.java
@@ -5,14 +5,27 @@
  */
 package ejb.session.stateless;
 
+import entity.PaymentTransaction;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 @Stateless
 public class TransactionSessionBean implements TransactionSessionBeanLocal {
 
     @PersistenceContext(unitName = "OptimalTravelPlan-ejbPU")
     private EntityManager em;
+    
+    @Override
+    public List<PaymentTransaction> retrieveAllPaymentTransaction(){
+        Query query = em.createQuery("SELECT p FROM PaymentTransaction p");
+        List<PaymentTransaction> paymentTransactions = query.getResultList();
+        for(PaymentTransaction p : paymentTransactions){
+            p.getPaymentAccount();
+        }
+        return paymentTransactions;
+    }
 
 }
