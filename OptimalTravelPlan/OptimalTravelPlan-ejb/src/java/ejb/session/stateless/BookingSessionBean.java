1300deb50f9d9a7522b700ec8bf9a83c64e93834
ryo <ryo200013@gmail.com>
2022-02-26 01:00:09 +0800

    CreateBooking

--- OptimalTravelPlan/OptimalTravelPlan-ejb/src/java/ejb/session/stateless/BookingSessionBean.java
+++ OptimalTravelPlan/OptimalTravelPlan-ejb/src/java/ejb/session/stateless/BookingSessionBean.java
@@ -6,22 +6,69 @@
 package ejb.session.stateless;
 
 import entity.Booking;
+import entity.Country;
+import entity.Service;
+import entity.Tag;
+import entity.TravelItinerary;
+import java.util.ArrayList;
 import java.util.List;
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceException;
 import javax.persistence.Query;
 import util.exception.BookingAlreadyConfirmedException;
 import util.exception.BookingNotFoundException;
+import util.exception.ConstraintViolationException;
+import util.exception.CountryNotFoundException;
+import util.exception.CreateNewBookingException;
+import util.exception.CreateNewServiceException;
+import util.exception.ServiceNotFoundException;
+import util.exception.TagNotFoundException;
+import util.exception.TravelItineraryNotFoundException;
+import util.exception.UnknownPersistenceException;
 
 @Stateless
 public class BookingSessionBean implements BookingSessionBeanLocal {
 
+    @EJB
+    private TravelItinerarySessionBeanLocal travelItinerarySessionBeanLocal;
+
+    @EJB
+    private ServiceSessionBeanLocal serviceSessionBeanLocal;
+
     @PersistenceContext(unitName = "OptimalTravelPlan-ejbPU")
     private EntityManager em;
 
-    //Create method needs to take in Service and TravelItinerary ID, possibly immediately create a transaction
-    //Does creating a traveliternerary mean that a booking must be created? What if a customer wants to have an itinerary just for reference?
+    public Long createBooking(Booking newBooking, Long serviceId, Long travelItineraryId) throws ConstraintViolationException, UnknownPersistenceException, CreateNewBookingException {
+        try {
+            Service service = serviceSessionBeanLocal.retrieveServiceById(serviceId);
+            TravelItinerary travelItinerary = travelItinerarySessionBeanLocal.retrieveTravelItineraryById(travelItineraryId);
+            newBooking.setService(service);
+            newBooking.setTravelItinerary(travelItinerary);
+            em.persist(newBooking);
+            travelItinerary.getBookings().add(newBooking);
+            service.getBookings().add(newBooking);
+            em.flush();
+
+            return newBooking.getBookingId();
+
+        } catch (PersistenceException ex) {
+            if (ex.getCause() != null && ex.getCause().getClass().getName().equals("org.eclipse.persistence.exceptions.DatabaseException")) {
+                if (ex.getCause().getCause() != null && ex.getCause().getCause().getClass().getName().equals("java.sql.SQLIntegrityConstraintViolationException")) {
+                    throw new ConstraintViolationException();
+                } else {
+                    throw new UnknownPersistenceException(ex.getMessage());
+                }
+            } else {
+                throw new UnknownPersistenceException(ex.getMessage());
+            }
+        } catch (ServiceNotFoundException | TravelItineraryNotFoundException ex) {
+            throw new CreateNewBookingException("Issue with provided serviceId or travelItinaryId!");
+        }
+    }
+
     @Override
     public Booking retrieveBookingById(Long bookingId) throws BookingNotFoundException {
         Booking booking = em.find(Booking.class, bookingId);
