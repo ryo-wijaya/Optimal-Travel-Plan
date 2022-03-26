472e278f1bf2dd15feca436ebe87b1fff18a377a
MingJun <e0540022@u.nus.edu>
2022-03-26 22:10:47 +0800

    quick fix

--- OptimalTravelPlan/OptimalTravelPlan-war/src/java/jsf/managedBean/RegisterPageManagedBean.java
+++ OptimalTravelPlan/OptimalTravelPlan-war/src/java/jsf/managedBean/RegisterPageManagedBean.java
@@ -5,9 +5,17 @@ package jsf.managedBean;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+import ejb.session.stateless.AccountSessionBeanLocal;
+import entity.Business;
+import java.io.IOException;
+import javax.ejb.EJB;
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import util.exception.PasswordNotAcceptedException;
+import util.exception.UnknownPersistenceException;
+import util.exception.UsernameAlreadyExistException;
 
 /**
  *
@@ -16,8 +24,9 @@ import javax.enterprise.context.RequestScoped;
 @Named(value = "registerPageManagedBean")
 @RequestScoped
 public class RegisterPageManagedBean {
-    
-    
+
+    @EJB
+    private AccountSessionBeanLocal accountSessionBeanLocal;
 
     private String username;
     private String password;
@@ -25,13 +34,76 @@ public class RegisterPageManagedBean {
     private String companyWebsite;
     private String companyNumber;
     private String companyAddress;
-    
-    
+
     public RegisterPageManagedBean() {
     }
-    
-    public void register() {
 
+    public String getCompanyNumber() {
+        return companyNumber;
     }
-    
+
+    public void setCompanyNumber(String companyNumber) {
+        this.companyNumber = companyNumber;
+    }
+
+    public String getCompanyAddress() {
+        return companyAddress;
+    }
+
+    public void setCompanyAddress(String companyAddress) {
+        this.companyAddress = companyAddress;
+    }
+
+    public void register() throws IOException {
+        try {
+            Business newBusiness = new Business(companyName, companyWebsite, companyNumber, companyAddress, username, password);
+            accountSessionBeanLocal.createNewAccount(newBusiness);
+
+            FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("isLogin", true);
+            FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("loggedInAccount", newBusiness);
+            FacesContext.getCurrentInstance().getExternalContext().getFlash().setKeepMessages(true);
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Registration Successful!", null));
+            FacesContext.getCurrentInstance().getExternalContext().redirect("./businessPageFolder/businessMain.xhtml");
+
+        } catch (PasswordNotAcceptedException ex) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Invalid Password!", null));
+        } catch (UsernameAlreadyExistException ex) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Username already exists!", null));
+        } catch (UnknownPersistenceException ex) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Registration Failed", null));
+        }
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getCompanyWebsite() {
+        return companyWebsite;
+    }
+
+    public void setCompanyWebsite(String companyWebsite) {
+        this.companyWebsite = companyWebsite;
+    }
+
 }
