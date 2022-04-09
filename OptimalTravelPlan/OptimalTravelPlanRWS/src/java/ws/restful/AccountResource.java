3c9d50d3d4f99f5bb0944ed089e2ac9f0276c2b6
ryo <ryo200013@gmail.com>
2022-04-10 00:28:30 +0800

    commit b4 pulling the thing

--- OptimalTravelPlan/OptimalTravelPlanRWS/src/java/ws/restful/AccountResource.java
+++ OptimalTravelPlan/OptimalTravelPlanRWS/src/java/ws/restful/AccountResource.java
@@ -6,6 +6,7 @@
 package ws.restful;
 
 import ejb.session.stateless.AccountSessionBeanLocal;
+import entity.Account;
 import entity.Booking;
 import entity.Customer;
 import entity.TravelItinerary;
@@ -17,6 +18,7 @@ import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Path;
@@ -26,19 +28,22 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 import util.exception.InvalidLoginCredentialException;
+import util.exception.PasswordNotAcceptedException;
+import util.exception.UnknownPersistenceException;
+import util.exception.UsernameAlreadyExistException;
 
 @Path("Account")
 public class AccountResource {
-    
+
     AccountSessionBeanLocal accountSessionBeanLocal = lookupAccountSessionBeanLocal();
-    
+
     @Context
     private UriInfo context;
-    
+
     public AccountResource() {
     }
-    
-    @Path("customerLogin")
+
+    @Path("CustomerLogin")
     @GET
     @Consumes(MediaType.TEXT_PLAIN)
     @Produces(MediaType.APPLICATION_JSON)
@@ -46,7 +51,7 @@ public class AccountResource {
         try {
             Customer customer = (Customer) accountSessionBeanLocal.login(username, password);
             System.out.println("********** CustomerResource.customerLogin(): Customer " + customer.getUsername() + " login remotely via web service");
-            
+
             customer.setPassword(null);
             List<TravelItinerary> travelItins = customer.getTravelItineraries();
             for (TravelItinerary ti : travelItins) {
@@ -61,10 +66,10 @@ public class AccountResource {
                     booking.getService().setCountry(null);
                     booking.getService().setBookings(new ArrayList<>());
                     booking.getService().setTags(new ArrayList<>());
-                    booking.getService().setBusiness(null);  
+                    booking.getService().setBusiness(null);
                 }
             }
-            
+
             return Response.status(Status.OK).entity(customer).build();
         } catch (InvalidLoginCredentialException ex) {
             return Response.status(Status.UNAUTHORIZED).entity(ex.getMessage()).build();
@@ -72,7 +77,26 @@ public class AccountResource {
             return Response.status(Status.INTERNAL_SERVER_ERROR).entity(ex.getMessage()).build();
         }
     }
-    
+
+    @PUT
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response createCustomerAccount(@QueryParam("username") String username, @QueryParam("password") String password, 
+            @QueryParam("name") String name, @QueryParam("mobile") String mobile, @QueryParam("passportNumber") String passportNumber, 
+            @QueryParam("email") String email, @QueryParam("vaccinationStatus") Boolean vaccinationStatus) {
+        try {
+            Long id = accountSessionBeanLocal.createNewAccount(new Customer(name, mobile, passportNumber, email, vaccinationStatus, username, password));
+            System.out.println("Customer Account created with ID: " + id + " remotely via web service");
+
+
+            return Response.status(Response.Status.OK).entity(id).build();
+        } catch (PasswordNotAcceptedException | UsernameAlreadyExistException | UnknownPersistenceException ex) {
+            return Response.status(Response.Status.BAD_REQUEST).entity(ex.getMessage()).build();
+        } catch (Exception ex) {
+            return Response.status(Status.INTERNAL_SERVER_ERROR).entity(ex.getMessage()).build();
+        }
+    }
+
     private AccountSessionBeanLocal lookupAccountSessionBeanLocal() {
         try {
             javax.naming.Context c = new InitialContext();
@@ -82,5 +106,5 @@ public class AccountResource {
             throw new RuntimeException(ne);
         }
     }
-    
+
 }
