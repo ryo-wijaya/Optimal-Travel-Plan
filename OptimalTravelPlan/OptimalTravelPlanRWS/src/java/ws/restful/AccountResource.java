7d059cae3923ce9cc2db666653872b8daff41887
ryo <ryo200013@gmail.com>
2022-04-10 01:13:28 +0800

    AccountResource

--- OptimalTravelPlan/OptimalTravelPlanRWS/src/java/ws/restful/AccountResource.java
+++ OptimalTravelPlan/OptimalTravelPlanRWS/src/java/ws/restful/AccountResource.java
@@ -18,6 +18,7 @@ import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
@@ -27,6 +28,9 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
+import util.exception.AccountDisabledException;
+import util.exception.AccountNotFoundException;
+import util.exception.ChangePasswordException;
 import util.exception.InvalidLoginCredentialException;
 import util.exception.PasswordNotAcceptedException;
 import util.exception.UnknownPersistenceException;
@@ -43,7 +47,7 @@ public class AccountResource {
     public AccountResource() {
     }
 
-    @Path("CustomerLogin")
+    @Path("customerLogin")
     @GET
     @Consumes(MediaType.TEXT_PLAIN)
     @Produces(MediaType.APPLICATION_JSON)
@@ -64,7 +68,6 @@ public class AccountResource {
                     booking.getReview().setBooking(null);
                     booking.getService().setBookings(new ArrayList<>());
                     booking.getService().setCountry(null);
-                    booking.getService().setBookings(new ArrayList<>());
                     booking.getService().setTags(new ArrayList<>());
                     booking.getService().setBusiness(null);
                 }
@@ -78,20 +81,61 @@ public class AccountResource {
         }
     }
 
+    @Path("createCustomerAccount")
     @PUT
     @Consumes(MediaType.TEXT_PLAIN)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response createCustomerAccount(@QueryParam("username") String username, @QueryParam("password") String password, 
-            @QueryParam("name") String name, @QueryParam("mobile") String mobile, @QueryParam("passportNumber") String passportNumber, 
+    public Response createCustomerAccount(@QueryParam("username") String username, @QueryParam("password") String password,
+            @QueryParam("name") String name, @QueryParam("mobile") String mobile, @QueryParam("passportNumber") String passportNumber,
             @QueryParam("email") String email, @QueryParam("vaccinationStatus") Boolean vaccinationStatus) {
         try {
             Long id = accountSessionBeanLocal.createNewAccount(new Customer(name, mobile, passportNumber, email, vaccinationStatus, username, password));
             System.out.println("Customer Account created with ID: " + id + " remotely via web service");
 
-
             return Response.status(Response.Status.OK).entity(id).build();
         } catch (PasswordNotAcceptedException | UsernameAlreadyExistException | UnknownPersistenceException ex) {
             return Response.status(Response.Status.BAD_REQUEST).entity(ex.getMessage()).build();
+        } catch (Exception ex) {
+            return Response.status(Status.INTERNAL_SERVER_ERROR).entity(ex.getMessage()).build();
+        }
+    }
+
+    @Path("changePassword")
+    @POST
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response changePassword(@QueryParam("username") String username, @QueryParam("password") String password,
+            @QueryParam("newPassword") String newPassword) {
+        try {
+            Customer customer = (Customer) accountSessionBeanLocal.login(username, password);
+
+            accountSessionBeanLocal.changePassword(password, newPassword, customer.getAccountId());
+            System.out.println("Changed password occured at customer account: " + customer.getAccountId() + " remotely via web service");
+
+            return Response.status(Response.Status.OK).build();
+        } catch (InvalidLoginCredentialException | AccountDisabledException | ChangePasswordException | AccountNotFoundException ex) {
+            return Response.status(Status.UNAUTHORIZED).entity(ex.getMessage()).build();
+        } catch (PasswordNotAcceptedException ex) {
+            return Response.status(Response.Status.BAD_REQUEST).entity(ex.getMessage()).build();
+        } catch (Exception ex) {
+            return Response.status(Status.INTERNAL_SERVER_ERROR).entity(ex.getMessage()).build();
+        }
+    }
+    
+    @Path("forgetPasswordChange")
+    @POST
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response forgetPasswordChange(@QueryParam("username") String username, @QueryParam("password") String password) {
+        try {
+            Customer customer = (Customer) accountSessionBeanLocal.login(username, password);
+
+            accountSessionBeanLocal.forgetPasswordChange(customer.getAccountId());
+            System.out.println("Forget password change occured at customer account: " + customer.getAccountId() + " remotely via web service");
+
+            return Response.status(Response.Status.OK).build();
+        } catch (AccountDisabledException | InvalidLoginCredentialException ex) {
+            return Response.status(Status.UNAUTHORIZED).entity(ex.getMessage()).build();
         } catch (Exception ex) {
             return Response.status(Status.INTERNAL_SERVER_ERROR).entity(ex.getMessage()).build();
         }
