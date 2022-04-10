46165858f9162e8213c4a74aa6b9dd75e3dc9a48
ryo <ryo200013@gmail.com>
2022-04-10 22:17:06 +0800

    changed get to post

--- OptimalTravelPlan/OptimalTravelPlanRWS/src/java/ws/restful/ServiceResource.java
+++ OptimalTravelPlan/OptimalTravelPlanRWS/src/java/ws/restful/ServiceResource.java
@@ -23,6 +23,7 @@ import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Produces;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
 import javax.ws.rs.QueryParam;
@@ -201,7 +202,7 @@ public class ServiceResource {
     }
     
     @Path("retrieveAllActiveServiceByTags")
-    @GET
+    @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public Response retrieveAllActiveServiceByTags(ServiceByTagHandler dataWrapper) {
